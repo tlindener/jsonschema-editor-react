b08951b6fb81a5b9e12a336edbcc264fbf344cda
tlindener <tobias.lindener@gmail.com>
2021-01-10 18:18:03 +0100

    Update react

--- src/model/context.ts
+++ src/model/context.ts
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import {PropertyType} from '../utils'
 import {JsonSchemaType} from '../json-schema.types'
 
--- src/model/provider.tsx
+++ src/model/provider.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import * as R from 'ramda'
 import {SchemaContext} from './context'
 import {PropertyType, getDefaultSchema, DataType} from '../utils'
